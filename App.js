30f80d9474a6042562a7902c2b62cf8d5067dc0c
Alex Fomushkin <aleksefo@gmail.com>
2017-09-29 23:27:32 +0300

    CVSData presented as FlatList, styled a bit, modal skeleton added.

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, FlatList, Modal, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 import Papa from 'papaparse'
 
 export default class App extends React.Component {
+
+	state = {
+		modalVisible: false,
+	}
+
 	RateHourly = 3.75
 	RateEvening = 1.15
 
-	csvData = 'Person Name,Person ID,Date,Start,End\n' +
+	csvData = 'Person_Name,Person_ID,Date,Start,End\n' +
 		'Scott Scala,2,2.3.2014,6:00,14:00\n' +
 		'Janet Java,1,3.3.2014,9:30,17:00\n' +
 		'Scott Scala,2,3.3.2014,8:15,16:00\n' +
@@ -21,25 +26,20 @@ export default class App extends React.Component {
 		'Larry Lolcode,3,6.3.2014,5:00,10:00\n' +
 		'Larry Lolcode,3,30.3.2014,8:00,16:00'
 
+	setModalVisible(visible) {
+		this.setState({modalVisible: visible});
+	}
+
 	loadCSV() {
 		let parsed = Papa.parse(this.csvData, {
+		// let parsed = Papa.parse('./src/dataList.csv', {
+		// 	download: true,
+		// 	delimiter: '\t',
 			header: true,
-			dynamicTyping: false,
-			preview: 0,
-			encoding: "",
-			worker: false,
-			comments: false,
-			step: undefined,
-			complete: undefined,
-			error: undefined,
-			download: false,
 			skipEmptyLines: true,
-			chunk: undefined,
-			fastMode: undefined,
-			beforeFirstChunk: undefined,
-			withCredentials: undefined
 		})
 		console.log(parsed.data)
+		return parsed.data
 		// Papa.parse('path/fileName.csv', {
 		// 	download: true,
 		// 	delimiter: '\t',
@@ -49,15 +49,51 @@ export default class App extends React.Component {
 		// })
 	}
 
+
+
   render() {
     return (
-      <View style={styles.container}>
+      <View >
+	      <Modal
+		      style={{margin: 22}}
+		      animationType="slide"
+		      transparent={false}
+		      visible={this.state.modalVisible}
+		      onRequestClose={() => {alert("Modal has been closed.")}}
+	      >
+		      <View style={{margin: 22}}>
+			      <View>
+				      <Text>Hello World!</Text>
+
+				      <TouchableHighlight onPress={() => {
+					      this.setModalVisible(!this.state.modalVisible)
+				      }}>
+					      <Text>Hide Modal</Text>
+				      </TouchableHighlight>
+
+			      </View>
+		      </View>
+	      </Modal>
         <Text>Open up App.js to start working on your app!</Text>
 	      <Button
-		      onPress={() => this.loadCSV()}
+		      onPress={() => this.setModalVisible(true)}
 		      title="Calculate"
 		      color="#841584"
 		      accessibilityLabel="Learn more about this purple button"
+	      />
+	      <FlatList
+
+		      data={this.loadCSV()}
+		      renderItem={({item}) =>
+			      <View style={styles.container}>
+				      <Text style={{flex: 0.5, flexDirection: 'row',justifyContent: 'center', textAlign: 'right',alignItems: 'center',backgroundColor: 'blue',}}>{item.Person_ID}</Text>
+				      <Text style={{flex: 3}}>{item.Person_Name}</Text>
+				      <Text style={{flex: 2, textAlign: 'right',}}>{item.Date}</Text>
+				      <Text style={{flex: 1, textAlign: 'right', marginRight: 5,}}>{item.Start}</Text>
+				      <Text style={{flex: 1}}>{item.End}</Text>
+			      </View>
+
+		      }
 	      />
       </View>
     );
@@ -67,8 +103,10 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+	  flexDirection: 'row',
+    backgroundColor: '#ffd',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+
   },
 });
